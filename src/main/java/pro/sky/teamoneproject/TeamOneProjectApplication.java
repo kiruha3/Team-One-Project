9005b58dfbeb72118d0b4b4aa7c0cfe66f94789b
Answer <a.n.s.w.e.r130597@gmail.com>
2024-02-10 16:06:54 +0700

    Start of project

---
+++ src/main/java/pro/sky/teamoneproject/TeamOneProjectApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.teamoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamOneProjectApplication.class, args);
+	}
+
+}
